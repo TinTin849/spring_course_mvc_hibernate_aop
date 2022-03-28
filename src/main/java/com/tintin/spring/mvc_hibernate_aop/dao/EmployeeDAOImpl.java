e004ff3b4b130efb63ba6d048a0d46d48996b1d8
TinTin <tintin200227@gmail.com>
2022-03-28 11:13:18 +0300

    Initial commit

---
+++ src/main/java/com/tintin/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,59 @@
+package com.tintin.spring.mvc_hibernate_aop.dao;
+
+import com.tintin.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+// List<Employee> allEmployees = session.createQuery("FROM Employee"
+//                 , Employee.class).getResultList();
+
+        Query<Employee> query = session.createQuery("FROM Employee", Employee.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = session.get(Employee.class, id);
+
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<Employee> query = session.createQuery("DELETE FROM Employee WHERE id = :employeeId");
+        query.setParameter("employeeId", id);
+
+        query.executeUpdate();
+    }
+}
